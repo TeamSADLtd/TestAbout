767d87878d7c6f6426dfef9c15de6dfc48fbba76
Dmitry <team.sad.ltd@gmail.com>
2021-11-18 15:23:13 +0300

    1 commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+console.log(`env = ${debug(process.env)}`);
+console.log(`API_KEY = ${debug(process.env.API_KEY)}`);
+
+// ! МЕТОДЫ ***************************************************************************************
+// содержимое объекта ***************************
+function debug(obj = {}) {
+    return JSON.stringify(obj, null, 2);
+}
